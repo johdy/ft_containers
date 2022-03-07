a4da79fb636b54001298244e08203b61c0cc434c
John DYER <jdyer@e1r5p21.42.fr>
2022-03-07 17:58:25 +0100

    avancee reverse_iterators

--- utils.hpp
+++ utils.hpp
@@ -3,6 +3,16 @@
 # include <iostream>
 
 namespace ft {
+
+	template<class Iter>
+	struct iterator_traits {
+		typedef typename Iter::difference_type difference_type;
+		typedef typename Iter::value_type value_type;
+		typedef typename Iter::reference reference;
+		typedef typename Iter::pointer pointer;
+		typedef typename Iter::iterator_category iterator_category;
+	};
+
 	template <bool B, class T>
 	struct enable_if_c {
 	  typedef T type;
@@ -63,5 +73,52 @@ namespace ft {
 	};
 
 	template <class T> 
-	struct is_integral : public is_integral_c<T> {};}
+	struct is_integral : public is_integral_c<T> {};
+
+
+
+
+
+	template< class Iter >
+	class reverse_iterator {
+		typedef Iter iterator_type;
+		typedef typename ft::iterator_traits<Iter>::iterator_category iterator_category;
+		typedef	typename ft::iterator_traits<Iter>::value_type value_type;
+		typedef	typename ft::iterator_traits<Iter>::difference_type difference_type;
+		typedef typename ft::iterator_traits<Iter>::pointer pointer;
+		typedef	typename ft::iterator_traits<Iter>::reference reference;
+
+	private:
+		iterator_type _elem;
+
+	public:
+		reverse_iterator() : _elem(0) { }
+		explicit reverse_iterator( iterator_type x ) { this->_elem = x; }
+
+		template< class U >
+		reverse_iterator( const reverse_iterator<U>& other ) {
+			this->_elem = other.base();
+		}
+
+
+		iterator_type base() const { return (_elem); }
+		reference operator*() const {
+			iterator_type cpy = _elem;
+			return (*(--cpy));
+		}
+		reverse_iterator operator+ (difference_type n) const { return ( reverse_iterator(_elem - n) ); }
+		reverse_iterator& operator++() { _elem--; }
+		reverse_iterator operator++(int) {
+			reverse_iterator cpy = *this;
+			--_elem;
+			return (cpy);
+		}
+		reverse_iterator& operator+= (difference_type n) {
+			*this = *this + n;
+			return (*this);
+		}
+	};
+
+
+}
 #endif
