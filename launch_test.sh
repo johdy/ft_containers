a3b29b0523903006bc7caad6312908212f7105e4
John DYER <jdyer@e1r5p9.clusters.42paris.fr>
2022-07-19 18:54:37 +0200

    suppr

--- launch_test.sh
+++
@@ -1,5 +0,0 @@
-clang++ -g -std=c++98 -D NAMESPACE=ft test.cpp -Iiterators -I.
-./a.out > ft_test.txt
-clang++ -g -std=c++98 -D NAMESPACE=std test.cpp -Iiterators -I.
-./a.out > std_test.txt
-diff ft_test.txt std_test.txt
--- launch_test_map.sh
+++
@@ -1,5 +0,0 @@
-clang++ -g -std=c++98 -D NAMESPACE=ft test_map.cpp
-./a.out > ft_test.txt
-clang++ -g -std=c++98 -D NAMESPACE=std test_map.cpp
-./a.out > std_test.txt
-diff ft_test.txt std_test.txt
